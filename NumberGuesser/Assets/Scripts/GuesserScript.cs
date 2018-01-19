788265d86e7392fb6314b6e4f92421a8ad934b2b
Christopher Brough <cbrough4@gmail.com>
2018-01-19 09:53:28 -0700

    Added NextGuess function, randomizer, 666 check

--- NumberGuesser/Assets/Scripts/GuesserScript.cs
+++ NumberGuesser/Assets/Scripts/GuesserScript.cs
@@ -6,7 +6,8 @@ public class GuesserScript : MonoBehaviour {
 
     public int max;
     public int min;
-    public int guess;
+    private int guess;
+    public int count;
 
     // Use this for initialization
     private void Start () {
@@ -16,31 +17,57 @@ public class GuesserScript : MonoBehaviour {
         print("Pick a number between " + min + " and " + max);
 
         //Is the value GUESS
-        print("Is the number " + guess + "?");
+        NextGuess();
         //Instructions - Push these buttons
         print("Up arrow for higher, down arrow for lower, enter for correct.");
 
-
+        max = max + 1;
 	}
+
+    private void NextGuess()
+    {
+        count--;
+        guess = Random.Range(min, max);
+        //prior guess code: guess = (min + max) / 2;********
+        print("Is the number " + guess + "?");
+    }
 	
 	// Update is called once per frame
 	 void Update () {
         //Up arrow
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             min = guess;
-            guess = (min + max) / 2;
-            print("Is the number " + guess + "?");
+            NextGuess();
         }
 
         //Down Arrow
-        if (Input.GetKeyDown(KeyCode.DownArrow)){
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             max = guess;
-            guess = (min + max) / 2;
-            print("Is the number " + guess + "?");
+            NextGuess();
         }
+
         //Enter button
-        if (Input.GetKeyDown(KeyCode.KeypadEnter)) {
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //checks if the number is 666
+            if (guess == 666)
+            {
+                guess = 777;
+                print("you did this to me :(");
+                print("I'm changing your answer to 777 because you are mean :(((");
+                print("Your number is now " + guess + ".");
+                print("punk.");
+            }
+            else
             print("i'm very smurt :O");
+        }
+
+        //count check (checks if it's zero, and if so, player wins
+        if (count <= 0)
+            {
+            print("... I guess you won...");
             }
     }
 }
