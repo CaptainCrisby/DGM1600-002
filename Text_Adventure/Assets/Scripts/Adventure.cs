b3181fb7a0e6f60edefa2de100ccfa747fbadbfd
Christopher Brough <cbrough4@gmail.com>
2018-01-29 22:06:50 -0700

    Text Adventure Game Now Personalized.
    This updates the game to match my flow chart.
    Game will now read computer time and User folder, as well as be able to read a random soda name from a txt file. Yay!
    Todo: Typewriter effect? Backgrounds, sounds, etc.

--- Text_Adventure/Assets/Scripts/Adventure.cs
+++ Text_Adventure/Assets/Scripts/Adventure.cs
@@ -5,111 +5,381 @@ using UnityEngine.UI;
 
 public class Adventure : MonoBehaviour {
 
-    public enum States { cell, window, gate, bed };
-    public States currentState;
+    public enum States { lobby, hallway, mirror, hospital, smash, bedroom, drink, enter, noEnter, trumpet, attack, run, wristwatch, noDrink, noEat, eat, good, bad };
+    private States currentState;
     public Text textObject;
     public Text titleObject;
-
+    public bool key = false;
+    public string soda;
 
     // Use this for initialization
-    void Start() {
-        currentState = States.cell;
+    void Start()
+    {
+        currentState = States.lobby;
+
+        //Setting up the random soda from the txt file
+        string path = "Assets/sodas.txt";
+        string[] lines = System.IO.File.ReadAllLines(path);
+        soda = lines[Random.Range(0, lines.Length)];
     }
 
-    // Update is called once per frame
-    void Update() {
-        if (currentState == States.cell)
+        // Update is called once per frame
+        void Update() {
+
+        if (currentState == States.lobby)
         {
-            Cell();
+            Lobby();
         }
 
-        else if (currentState == States.bed)
+        else if (currentState == States.hallway)
         {
-            Bed();
+            Hallway();
         }
 
-        else if (currentState == States.gate)
+        else if (currentState == States.mirror)
         {
-            Gate();
+            Mirror();
         }
 
-        else if ((currentState == States.window))
+        else if ((currentState == States.hospital))
         {
-            Window();
+            Hospital();
+        }
+
+        else if ((currentState == States.smash))
+        {
+            Smash();
+        }
+
+        else if ((currentState == States.bedroom))
+        {
+            Bedroom();
+        }
+
+        else if ((currentState == States.drink))
+        {
+            Drink();
+        }
+
+        else if ((currentState == States.enter))
+        {
+            Cat();
+        }
+
+        else if ((currentState == States.noEnter))
+        {
+            NoCat();
+        }
+
+        else if ((currentState == States.trumpet))
+        {
+            Trumpet();
+        }
+
+        else if ((currentState == States.attack))
+        {
+            Attack();
+        }
+
+        else if ((currentState == States.run))
+        {
+            Run();
+        }
+
+        else if ((currentState == States.wristwatch))
+        {
+            Wristwatch();
+        }
+
+        else if ((currentState == States.noDrink))
+        {
+            NoDrink();
+        }
+
+        else if ((currentState == States.noEat))
+        {
+            NoEat();
+        }
+
+        else if ((currentState == States.eat))
+        {
+            Eat();
+        }
+
+        else if ((currentState == States.bad))
+        {
+            BadEnd();
+        }
+
+        else if ((currentState == States.good))
+        {
+            GoodEnd();
         }
 
     }
 
     // "\n" makes it so it displays on a new line. This is a bunch of stuff that runs depending on currentState's value.
 
-    private void Cell(){
-        print("Hello UVU number " + System.Environment.UserName + ". " +
-            "You are in a cell. It's gross and dark and stinky. \n" +
-            "There is a Window. \n" +
-            "There is a Gate. \n" +
-            "There is a Bed. \n" +
-            "Press W for Window, G for gate, B for bed.");
+    private void Lobby(){
         titleObject.text = "WUDDUP PIMPS IN THE CELL";
-        textObject.text = "Hello UVU number " + System.Environment.UserName + ". " +
-            "You are in a cell. It's gross and dark and stinky. \n" +
-            "There is a Window. \n" +
-            "There is a Gate. \n" +
-            "There is a Bed. \n" +
-            "Press W for Window, G for gate, B for bed.";
-        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
-        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
-        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
+        textObject.text = "I'm in a dimly lit room..." +
+            "\nThere are 2 doors, each with poorly written, illegible handwriting." +
+            "\nJust trying to make out these words makes my head hurt..." +
+            "\nLeft click for the first door, right click for the second door.";
+        if (Input.GetKeyDown(KeyCode.Mouse0)) { currentState = States.hallway; }
+        if (Input.GetKeyDown(KeyCode.Mouse1)) { currentState = States.bedroom; }
     }
 
-    private void Bed()
+    private void Hallway()
     {
-        print("You are in a bed. It's gross and dark and stinky. " +
-            "Yoodoo. \n" +
-            "Yodo. \n" +
-            "You are now Frodo. \n" +
-            "Press W for Window, G for gate, C for cell.");
-        textObject.text = "You are in a bed. It's gross and dark and stinky. " +
-            "Yoodoo. \n" +
-            "Yodo. \n" +
-            "You are now Frodo. \n" +
-            "Press W for Window, G for gate, C for cell.";
-        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
-        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
-        if (Input.GetKeyDown(KeyCode.C)) { currentState = States.cell; }
+        textObject.text = "I can see there's a peculiarly long hallway." +
+            "\nSeems pretty ominous." +
+            "\nAt the very end there's a locked door...";
+        if (key == true)
+        {
+            textObject.text = "I use the key I just received..." +
+                "\nThe door unlocked!";
+            currentState = States.mirror;
+        }
+        else if (key == false)
+        {
+            textObject.text = "I don't have anything that could unlock this at this time." +
+                "\nBest I head back.";
+            currentState = States.lobby;
+        }
     }
 
-    private void Gate()
+    private void Mirror()
     {
-        print("You found a gate. It's gross and dark and stinky. " +
-            "Yoodoo. \n" +
-            "Yodo. \n" +
-            "You are now Frodo. \n" +
-            "Press C for Cell, W for window, B for bed.");
-        textObject.text = "You found a gate. It's gross and dark and stinky. " +
-            "Yoodoo. \n" +
-            "Yodo. \n" +
-            "You are now Frodo. \n" +
-            "Press C for Cell, W for window, B for bed.";
-        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.window; }
-        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
-        if (Input.GetKeyDown(KeyCode.C)) { currentState = States.cell; }
+        textObject.text = "There's an image of me. " +
+            "\nIt's showcasing some of my most memorable events... " +
+            "\nLike the time I got an A to show Mom. " +
+            "\nOr scoring that goal in P.E. " +
+            "\nAnd even meeting that special someone. " +
+            "\nAn overwhelming emotion starts to sweep over myself. " +
+            "\nSuddenly, these memories fade into the mirror. " +
+            "\nThe silhouette has appeared. " +
+            "\nHe's holding an object with an obvious trigger and handle at hand. " +
+            "\nThe room has, for some reason, has begun to snow. " +
+            "\nWhat do I do?" +
+            "\nLeft click to smash the mirror, and right click to hold back. ";
+        if (Input.GetKeyDown(KeyCode.Mouse0)) { currentState = States.smash; }
+        if (Input.GetKeyDown(KeyCode.Mouse1)) { currentState = States.hospital; }
     }
 
-    private void Window()
+    private void Hospital()
     {
-        print("You found a Window. It's gross and dark and stinky. " +
-            "Yoodoo. \n" +
-            "Yodo. \n" +
-            "You are now Frodo. \n" +
-            "Press C for Cell, G for Gate, B for Bed.");
-        textObject.text = "You found a Window. It's gross and dark and stinky. " +
-            "Yoodoo. \n" +
-            "Yodo. \n" +
-            "You are now Frodo. \n" +
-            "Press C for Cell, G for Gate, B for Bed.";
-        if (Input.GetKeyDown(KeyCode.G)) { currentState = States.gate; }
-        if (Input.GetKeyDown(KeyCode.B)) { currentState = States.bed; }
-        if (Input.GetKeyDown(KeyCode.C)) { currentState = States.cell; }
+        textObject.text = "I hold back as the silhouette slowly puts away its object. " +
+            "\nIt nods its head at me as my vision began to fade... " +
+            "\n... " +
+            "\n... " +
+            "\nH-'s -akin- up!" +
+            "\nDo--or! -e's a-ke!" +
+            "\nA familiar white room surrounds me. " +
+            "\nHey! You're " + System.Environment.UserName + ", right?" +
+            "\n... " +
+            "\nI see. " +
+            "\nWell, you see, you've been in a coma " + System.Environment.UserName + " . " +
+            "\nI remember that name. " +
+            "\nMy family walks into the room, embracing the mere sight of my eyes open. " +
+            "\nI've missed them for such a long time. " +
+            "\nI wonder how much I've missed?" +
+            "\nEither way, I'm glad to be back home now. Safe from whatever weird dream that was. ";
+        currentState = States.good;
+    }
+
+    private void Smash()
+    {
+        textObject.text = "I threw my fist at the mirror as hard as I could!" +
+            "\nIt shattered. " +
+            "\nAn overwhelming sense of pride washes over me. " +
+            "\nAdmittedly, I can't help but laugh about the situation. " +
+            "\nAnd the laughter slowly gets louder. " +
+            "\nAnd louder. " +
+            "\n... " +
+            "\n... " +
+            "\nH-'s -akin- up!" +
+            "\nDo--or! -e's a-ke!" +
+            "\nAn uncomfortably bright, white room surrounds me. " +
+            "\nHey! You're " + System.Environment.UserName + ", right?" +
+            "\n... " +
+            "\nI see. " +
+            "\nWell, you see, you've been in a coma " + System.Environment.UserName + " . " +
+            "\nThat's weird" +
+            "\nI don't remember that name. " +
+            "\nA family walks into the room, embracing the mere sight of my eyes open. " +
+            "\nBut I'm not sure I recognize them. " +
+            "\nAnd I don't think I ever will. ";
+        currentState = States.bad;
+    }
+
+    private void Bedroom()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I open the door carefully... " +
+            "\nIt's a bedroom. " +
+            "\nThe bed looks super comfy, but you resist temptation. " +
+            "\nStrangely enough, a Giant can of " + soda + " sits in the room. " +
+            "\nPress D to drink or N to not drink. ";
+        if (Input.GetKeyDown(KeyCode.D)) { currentState = States.drink; }
+        if (Input.GetKeyDown(KeyCode.N)) { currentState = States.noDrink; }
+    }
+
+    private void Drink()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I feel rejuvenated!" +
+            "\nA giant cat head crashes through the wall with its mouth wide open. " +
+            "\nPress E to enter, N to not. ";
+        if (Input.GetKeyDown(KeyCode.E)) { currentState = States.enter; }
+        if (Input.GetKeyDown(KeyCode.N)) { currentState = States.noEnter; }
+    }
+
+    private void Cat()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "You decide to go inside the cat's mouth. " +
+            "\nAs you walk into the deepest and darkest corners of its mouth, a silhoutte appears. " +
+            "\nBefore you can get a good look at it... " +
+            "\n*SMACK*" +
+            "\nThat was weird?" +
+            "\nThat noise sounded like a loud thud... " +
+            "\nHow strange. " +
+            "\nIt almost sounded like the noise came from me. " +
+            "\nNot even a moment passes and the realization hit me... " +
+            "\nI've been struck on the head. " +
+            "\nA have of heat penetrated my head as I rapidly grasped the now throbbing ache. " +
+            "\nQuickly, my body grew numb as it slowly wobbed toward the floor... ";
+        currentState = States.bad;
+    }
+
+    private void NoCat()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I decided not to enter in the cat's mouth. " +
+            "\nThe room's quiet air is suddenly interrupted as the cat began to move its jaw. " +
+            "\nThanks man it remarked. " +
+            "\nHowever, just as quickly as it spoke, the silhouette appeared. " +
+            "\nAnd before I know it, there is a trumpet and wristwatch in front of me. " +
+            "\nPress T for the trumpet and W for the wristwatch. ";
+        if (Input.GetKeyDown(KeyCode.T)) { currentState = States.trumpet; }
+        if (Input.GetKeyDown(KeyCode.W)) { currentState = States.wristwatch; }
+    }
+
+    private void Trumpet()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I grab the trumpet and attempt to play it quickly. " +
+            "\nOf course, however, I have no clue how to play the trumpet. " +
+            "\nDespite my lack of knowledge, I attempt to serenade the silhouette. " +
+            "\nHe remains unimpressed. " +
+            "\nPress A to attack him, press R to run away. ";
+        if (Input.GetKeyDown(KeyCode.A)) { currentState = States.attack; }
+        if (Input.GetKeyDown(KeyCode.R)) { currentState = States.run; }
+    }
+
+    private void Attack()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "The silhouette moves at a blinding speed. " +
+            "\n*Woosh!*" +
+            "\nSome sort of bludgeon just barely skims over my head. " +
+            "\nEven with my quick evasive skills, I can't find an opening to hit him!" +
+            "\nThe small space the room gave me works against me in this fight. " +
+            "\nAnd in a moment's notice... " +
+            "\n*SMACK*" +
+            "\nThat was weird?" +
+            "\nThat noise sounded like a loud thud... " +
+            "\nHow strange. " +
+            "\nIt almost sounded like the noise came from me. " +
+            "\nNot even a moment passes and the realization hit me... " +
+            "\nI've been struck on the head. " +
+            "\nA have of heat penetrated my head as I rapidly grasped the now throbbing ache. " +
+            "\nQuickly, my body grew numb as it slowly wobbled toward the floor... ";
+        currentState = States.bad;
+    }
+
+    private void Run()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "No way I can take on this guy!" +
+            "\nI run back into the lobby, forgetting everything that just happened. ";
+        currentState = States.lobby;
+    }
+
+    private void Wristwatch()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I pick up the wriswatch to tell the time. " +
+            "\nThe current time is " + System.DateTime.Now + ". " +
+            "\nThe silhouette readjusts his stance, seemingly pleased with this response. " +
+            "\nHe's dropped something... " +
+            "\nA... key?" + (key = true) +
+            "\nAs the silhouette disappears, I think about what this key could go to. " +
+            "\nIt doesn't seem like there's anything important here anymore. " +
+            "\nSo let's try visiting the other door in the lobby. ";
+        currentState = States.hallway;
+    }
+
+    private void NoDrink()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I don't feel like drinking it right now. " +
+            "\nHowever, unease begins to fill inside of me as the Cola disappears. " +
+            "\nSuddenly, " + soda + " man, the brand's mascot, pounds his way through the wall!" +
+            "\nUnfortunately, this is the end of the line. " +
+            "\nCola Man draws back his fist, readying his infamous punch. " +
+            "\nSuddenly, Cola Man returns to the caramel colored ooze he is. " +
+            "\nHe leaves behind a trinket. " +
+            "\nIt's a picture of you? That's weird. " +
+            "\nThere's an urge to eat it... it might taste like cola!" +
+            "\nPress E to eat it, press N to not eat it. ";
+        if (Input.GetKeyDown(KeyCode.E)) { currentState = States.eat; }
+        if (Input.GetKeyDown(KeyCode.N)) { currentState = States.noEat; }
+    }
+
+    private void NoEat()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I don't think it's a good idea to eat this thing. " +
+            "\n... " +
+            "\nIt began to glow a mysterious light. " +
+            "\nIt's turned into a key!" + (key = true) +
+            "\nDoesn't seem like this room needs it. " +
+            "\nLooks like we'll have to try the other door in the lobby. ";
+        currentState = States.hallway;
+    }
+
+    private void Eat()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "I figure I might as well eat it. " +
+            "\nWhat else is it supposed to do? " +
+            "\nI popped it into my mouth. " +
+            "\n... " +
+            "\nYou know?" +
+            "\nIt actually doesn't taste half bad. " +
+            "\nI have an urge to chew it, but as soon as I do... " +
+            "\n*crunch*" +
+            "\nThe trinket, in a strange event, turned into a malleable gold key as soon as I chewed. " +
+            "\nThe key is now broken in half. " +
+            "\nThe door from where I came from is now closed shut and has locked itself." +
+            "\nWithout much left to do, I guess I'll just have to sit and wait..." +
+            "\nAnd wait..." +
+            "\nAnd wait.....................";
+        currentState = States.bad;
+    }
+
+    private void BadEnd()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "\nit's the bad ending mofos";
+    }
+
+    private void GoodEnd()
+    {
+        titleObject.text = "WUDDUP PIMPS IN THE CELL";
+        textObject.text = "\nit's the good ending mofos";
     }
 
 }
