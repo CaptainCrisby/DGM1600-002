528fb2acb2d2d221d01fba643ea4281165a872f2
Christopher Brough <cbrough4@gmail.com>
2018-02-09 10:16:56 -0700

    Brick Breaker more health
    Also fixed some weird error text adventure

---
+++ BrickBreaker/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager instance = null;
+
+    // Use this for initialization
+    void Awake () {
+        //Check if instance already exists - Singleton
+        if (instance == null)       //If the instance doesn't have anything assigned.
+        {
+            instance = this;        //Set the instance.
+        }
+        //If it already exists destroy it
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        //Don't destroy it when loading, though.
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+	
+}
