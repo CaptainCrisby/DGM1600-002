fec1a49d201b065db47ef4aa93785bd3524e8408
Christopher Brough <cbrough4@gmail.com>
2018-02-14 10:02:41 -0700

    Brick Breaker
    Working on fixing sound so it alters pitch based on what it hits

---
+++ BrickBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public AudioClip bounce;
+    AudioSource audioSource;
+    public GameObject Paddle;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if it's colliding with an object named "paddle"
+        if(collision.gameObject == Paddle)
+        {
+            audioSource.pitch = -55;
+            AudioSource.PlayClipAtPoint(bounce, new Vector3(0, 0, 0));
+        }
+        else //otherwise just don't mess with the pitch
+        {
+            audioSource.pitch = 0;
+            AudioSource.PlayClipAtPoint(bounce, new Vector3(0, 0, 0));
+        }
+
+        //AudioSource.PlayClipAtPoint(bounce, new Vector3(0,0,0));
+    }
+
+}
--- BrickBreaker/Assets/Scripts/Paddle.cs
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
+    public bool autoPlay;
+    private GameObject ball;
 
-	// Update is called once per frame
-	void Update () {
+
+    private void Start()
+    {
+        ball = FindObjectOfType<Ball>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
         //get current paddle position
         Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
 
-        //audiosource.playclipatpoint (when colliding)
 
-        //get horizontal position
-        float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16 - 8;
+        if (autoPlay)
+        {
+            paddlePosition.x = ball.transform.position.x;
+        }
+        else
+        {
+            //get horizontal position
+            float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16 - 8;
 
-        paddlePosition.x = mousePositionInBlocks;
+            paddlePosition.x = mousePositionInBlocks;
+        }
 
         //set this paddle object to the saved position
         gameObject.transform.position = paddlePosition;
