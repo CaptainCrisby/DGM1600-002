da3f4f735ae8ed9f9a605c2ccf54957ae1d88d2b
Christopher Brough <cbrough4@gmail.com>
2018-02-21 10:19:50 -0700

    Brick Breaker - Brick Check

--- BrickBreaker/Assets/Scripts/GameManager.cs
+++ BrickBreaker/Assets/Scripts/GameManager.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
+    public static int brickCount;
 
-    // Use this for initialization
     void Awake()
     {
         //Check if instance already exists - Singleton
@@ -27,7 +27,16 @@ public class GameManager : MonoBehaviour {
     //loadin' levels
     public void LoadLevel(string level)
     {
+        brickCount = 0;
         SceneManager.LoadScene(level);
     }
+
+    public void LoadNextLevel()
+    {
+        brickCount = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+
 
 }
--- BrickBreaker/Assets/Scripts/Health.cs
+++ BrickBreaker/Assets/Scripts/Health.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Health : MonoBehaviour {
     public int health;
 
+    private void Awake()
+    {
+        GameManager.brickCount++;
+        print(GameManager.brickCount);
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         health--;
@@ -13,6 +20,12 @@ public class Health : MonoBehaviour {
         if (health <= 0)
         {
             Destroy(gameObject);
+            GameManager.brickCount--;
+            print(GameManager.brickCount);
+            if (GameManager.brickCount == 0)
+            {
+                FindObjectOfType<GameManager>().LoadNextLevel();
+            }
         }
     }
 
