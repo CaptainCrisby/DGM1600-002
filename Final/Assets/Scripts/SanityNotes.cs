a959101dbea20b176187646719b42eacae81c708
Christopher Brough <cbrough4@gmail.com>
2018-03-24 21:33:28 -0600

    Final-Sanity Notes
    Added sanity notes for reference

--- Final/Assets/Scripts/SanityNotes.cs
+++ Final/Assets/Scripts/SanityNotes.cs
@@ -1,13 +1,8 @@
-/*      if (dialogueText.text.Contains("playerName"))
-      {
-        dialogueText.text.Replace("playerName", yourName);
-      }
-
-      Incredibly annoying! Been trying this code for a while on the
-      "Dialogue Manager" script, but the way its programmed doesn't support variables being typed out...
-      What I'm trying to do is have the script detect if the string "playerName" is in a sentence,
-      followed by the script automatically replacing it with a name variable I've setup with the
-      NameManager object (which will be in every scene).
-      It almost works if placed toward the end of "TypeSentence", only issue is that it first types out
-      'playerName' before randomly turning into your name.
-      I'll look more into this tomorrow.*/
+/* Need to fix the animations (I forgot to keyframe all the bones...)
+ *
+ *
+ *
+ *
+ *
+ *
+ */
